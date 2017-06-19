2198f6ed741a5ea309fd5eed0a67cf95c2ec9f70
C. Poda <clp302@poda.net>
2017-06-18 18:19:21 -0700

    Use bokeh output_file() instead of render_template().
    This change presents the output plot in a browser window; the
    prior code was not showing a plot in my environment.

--- Read_and_plot_IBM_Prices_quantl.py
+++ Read_and_plot_IBM_Prices_quantl.py
@@ -1,16 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jan 13 13:28:55 2017
-
-@author: Steve Siegel
-"""
 from flask import Flask, render_template, request, redirect
 import pandas as pd
 import requests
 import datetime as dt
-from bokeh.plotting import figure
+from bokeh.plotting import figure, output_file, show
 from bokeh.embed import components
-import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
@@ -18,17 +11,18 @@ app = Flask(__name__)
 def main():
     return redirect('/index')
 
-#@app.route('/index', methods=['GET','POST'])
-@app.route('/index', methods=['GET'])
+
+@app.route('/index', methods=['GET','POST'])
 def index():
     return render_template('index.html')
+
 
 def getUserInput():
     #get the stock ticker and make it uppercase
     stockTicker = request.form['ticker'].upper()
     #turn checked features into a list
     features = request.form.getlist('feature')
-    print (stockTicker)
+    
     return stockTicker, features
 
 def processData(stockTicker):
@@ -40,73 +34,55 @@ def processData(stockTicker):
     startDate = (now - dt.timedelta(days=90)).strftime('%Y-%m-%d')
     
     #get data from QUANDL
-    #link = 'https://www.quandl.com/api/v3/datasets/WIKI/'+stockTicker+'.json?start_date='+startDate+'&end_date='+endDate+'&order=asc'
-    #jsonData = requests.get(link)
-    ibm_prices_df = pd.read_csv('https://www.quandl.com/api/v3/datasets/WIKI/IBM.csv')
-    ibm_prices_df.to_csv('/home/ann/tmp/prices0.csv', float_format='%4.2f', index=False)
+    link = 'https://www.quandl.com/api/v3/datasets/WIKI/'+stockTicker+'.json?start_date='+startDate+'&end_date='+endDate+'&order=asc'
+    #D print("#D, link: ", link)
+    jsonData = requests.get(link)
+    
     # convert jsonData into dataframe
-    #dfData = pd.DataFrame(jsonData.json())
+    dfData = pd.DataFrame(jsonData.json())
+    #D print("#D, dfData.head(): ", dfData.head())
     
     #extract relevant data
-    #values = dfData.ix['data', 'dataset'] #list of list
-    #columnNames = dfData.ix['column_names','dataset'] #list of string
-    #relevantData = pd.DataFrame(values, columns = columnNames)
+    values = dfData.ix['data', 'dataset'] #list of list
+    columnNames = dfData.ix['column_names','dataset'] #list of string
+    relevantData = pd.DataFrame(values, columns = columnNames)
     
     #set date as index and convert date to actual date format
-    #relevantData = relevantData.set_index(['Date'])
-    #relevantData.index = pd.to_datetime(relevantData.index)
+    relevantData = relevantData.set_index(['Date'])
+    relevantData.index = pd.to_datetime(relevantData.index)
     
-    #return dfData
+    return relevantData
 
 
+def createPlot(data, features, stockTicker):
+    # Create a Bokeh plot from the dataframe
+    plot = figure(x_axis_type = "datetime")
+    plot.title.text = 'Stock Ticker: ' + stockTicker
+    if 'Open' in features:
+        plot.line(data.index, data['Open'], color='green', legend='Opening Price')
+    if 'High' in features:
+        plot.line(data.index, data['High'], color='red', legend='Highest Price')
+    if 'Low' in features:
+        plot.line(data.index, data['Low'], color ='blue', legend = 'Lowest Price')
+    if 'Close' in features:
+        plot.line(data.index, data['Close'], color='black', legend='Closing Price')
+    
+    return plot
 
-#@app.route('/result',methods=['GET','POST'])
-@app.route('/result',methods=['GET'])
+
+@app.route('/result',methods=['GET','POST'])
 def result():
     stockTicker, features = getUserInput()
     data = processData(stockTicker)
-   
+    print("#D, data.head(): ", data.head())
     
-    #return render_template('bokeh.html', script=script, div=div)
+    # Send output to static HTML file.
+    # The plot should appear in the browser.
+    output_file("bokeh.html")
+    p = createPlot (data, features, stockTicker)
+    show(p)
+    
+    return 'OK'
 	
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-    
-#import pandas as pd
-#3import matplotlib.pyplot as plt
-#from datetime import datetime
-
-#symbol = input("enter stock symbol:  ")
-#start_date = input("enter start date YYYY-MM-DD: ")
-#stop_date = input("enter stop date:  YYYY-MM-DD: ")
-                   
-
-#ibm_prices_df = pd.read_csv('http://ichart.finance.yahoo.com/table.csv?s='+ symbol)
-#ibm_prices_df.to_csv('/tmp/prices0.csv', float_format='%4.2f', index=False)
-
-
-
-#start = ibm_prices_df.Date > start_date #'2016-12-1' 
-#stop = ibm_prices_df.Date < stop_date #'2017-1-1'
-#pred_sr = start & stop
-
-#print(pred_sr)
-#prices0_df = ibm_prices_df.copy()[['Date','Adj Close']][pred_sr]
-
-#prices1_df = prices0_df.sort_values('Date')
-
-#prices2_df = prices1_df.set_index('Date')
-
-#prices1_df.columns = ['date_s','price']
-
-#prices2_df = prices1_df.copy()[['date_s','price']]
-
-#date_dt_l = [datetime.strptime(day_s, '%Y-%m-%d') for day_s in prices2_df.date_s]
-#prices2_df['Date'] = date_dt_l
-
-#prices3_df = prices2_df.copy()[['Date','price']]
-#prices4_df = prices3_df.set_index('Date')
-
-#prices4_df.plot.line(title=symbol + " Prices")
-#plt.savefig('IBM_Prices.png')
-
